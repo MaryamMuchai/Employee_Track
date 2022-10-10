176c75e3e3ef963381e6a59d90a41d0b70b59558
Marya <maryammuchai@gmail.com>
2022-10-10 09:45:25 +0300

    creating the employee model

--- emp/models.py
+++ emp/models.py
@@ -2,5 +2,15 @@ from django.db import models
 from django.conf import settings
 
 # Create your models here.
-class Emp(models.Model)
+class Emp(models.Model):
+
+    emp_id = models.CharField(max_length=100, primary_key=True)
+    emp_name = models.CharField(max_length=100)
+    emp_dept = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE,related_name="emp")
+    emp_salary = models.FloatField()
+    date_join = models.DateField()
+    emp_address = models.TextField()
+
+    def __str__(self) -> str:
+        return self.emp_name
     
